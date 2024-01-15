2e234a90e3d2d87f6ed7ee22a9823a304385dfd6
Dominic Maia <dominicmaia@hotmail.com>
2024-01-15 21:21:17 +0000

    .

---
+++ Gestor.java
@@ -0,0 +1,18 @@
+public class Gestor {
+    private String login;
+    private String password;
+    private String nome;
+    private boolean estado;
+    private String email;
+    private String tipo;
+
+    public Gestor(String llogin, String ppassword, String nnome, boolean eestado, String eemail, String ttipo, int nnif, String mmorada, int ttelefone) {
+        this.login = llogin;
+        this.password=ppassword;
+        this.nome=nnome;
+        this.estado=eestado;
+        this.email=eemail;
+        this.tipo=ttipo;
+
+    }
+}
