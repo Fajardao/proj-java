75f26c597daa7f5a790440c11571fc0891822c57
Dominic Maia <dominicmaia@hotmail.com>
2024-01-16 01:27:02 +0000

    .

--- Excipiente.java
+++ Excipiente.java
@@ -1,3 +1,13 @@
 public class Excipiente {
+    private String designacao;
+    private String classificacaoInfarmed;
+    private int quantidade;
     
+    public Excipiente(String ddesignacao, String cclassificacaoInfarmed,int qquantidade){
+        this.designacao=ddesignacao;
+        this.classificacaoInfarmed=cclassificacaoInfarmed;
+        this.quantidade=qquantidade;
+    }
+    
+
 }
